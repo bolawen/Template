0aa5ce73310f52643cf653c6511fcadafa4efc16
zhangwenqiang <335303383@qq.com>
2021-06-15 22:04:03 +0800

    推送

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
         '@typescript-eslint/no-var-requires': 'off', //是否禁止在 import 语句之外使用 require 语句
         '@typescript-eslint/explicit-module-boundary-types': 'off', //是否要求导出函数和类的公共类方法的显式返回和参数类型
         '@typescript-eslint/triple-slash-reference': 'off', //是否禁止使用/// <reference path="" />，/// <reference types="" />或/// <reference lib="" />指令
+        '@typescript-eslint/no-inferrable-types': 0, //是否开启推断类型
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         semi: 2, //分号 检测开关
