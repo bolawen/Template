0b79ffe12ddaf195d3347d2519acba982e3b1303
szhangwenqiang_v@didichuxing.com <szhangwenqiang_v@didiglobal.com>
2022-06-22 16:12:00 +0800

    feat(src): 推送

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import logo from '@/assets/svg/logo.svg';
 
 function App() {
-    const [title,setTitle] = useState<string>('Vite-React 模板');
-    const name:string = 200;
-    const age:number = 'kkk';
-    const str:number = "kk";
+    const [title, setTitle] = useState<string>('Vite-React 模板');
+    const name: string = 'st';
+    const age: number = 200;
     return (
         <div>
             <h3>{title}</h3>
-            <img src={logo} alt=""/>
+            <img src={logo} alt="" />
         </div>
     );
 }
