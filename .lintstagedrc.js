9689c389f6989232073ecab2c1a1d3f3b6941b52
szhangwenqiang_v@didichuxing.com <szhangwenqiang_v@didiglobal.com>
2022-06-22 13:27:33 +0800

    测试

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,4 @@
-const fs = require('fs');
-const generateTSConfig = (fileName, type) => {
-    const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf8'));
-    const include = ['src/**/*.d.ts', 'auto-imports.d.ts', 'components.d.ts', ...fileName].filter((file) => !file.includes('TabContent'));
-    tsconfig.include = include;
-    fs.writeFileSync('tsconfig.lint.json', JSON.stringify(tsconfig));
-    return `${type} --noEmit --project tsconfig.lint.json`;
-};
-
 module.exports = {
-    'src/**/*{js,jsx,ts,tsx,vue}': ['prettier --write', 'eslint --fix'],
-    'src/**/*{css,less,scss,vue}': ['stylelint --fix'],
-    'src/**/*.{ts,tsx}': [(fileName) => generateTSConfig(fileName, 'tsc')],
+    'src/**/*.{ts,tsx}': ['eslint --fix', 'git add'],
+    'src/**/*.{js,jsx}': ['eslint --fix', 'git add'],
 };
