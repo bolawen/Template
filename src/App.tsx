3e2b8f1eb65b361b11cef4805937587005b992a3
szhangwenqiang_v@didichuxing.com <szhangwenqiang_v@didiglobal.com>
2022-06-22 12:58:49 +0800

    测试

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {useState} from 'react';
 import logo from '@/assets/svg/logo.svg';
 
 function App() {
-    const [title] = useState<string>('Vite-React 模板');
+    const [title,setTitle] = useState<string>('Vite-React 模板');
     return (
         <div>
             <h3>{title}</h3>
