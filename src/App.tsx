22c39ff8bdbcac761df3907c380f315e93fb8d1c
szhangwenqiang_v@didichuxing.com <szhangwenqiang_v@didiglobal.com>
2022-06-22 16:57:56 +0800

    feat(src): 推送

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,6 @@ function App() {
     return (
         <div>
             <h3>{title}</h3>
-            <div>
-                kkkkkkkkkkkkfdfdfdkkkkkfdfdfdifjdifjdisfjidsjfidsjfidsjfidsjfidjs
-            </div>
             <img src={logo} alt="" />
         </div>
     );
